dd9514986f0e517559bda1e277eca96116f9f444
William To <towilliam03@gmail.com>
2020-02-09 17:05:22 -0500

    Add some panning to distorted sphere

---
+++ sketches/2020.02.09.js
@@ -0,0 +1,92 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require('three');
+
+// Include any additional ThreeJS examples below
+require('three/examples/js/controls/OrbitControls');
+
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: 'webgl'
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas
+  });
+
+  // WebGL background color
+  renderer.setClearColor('#fff', 1);
+
+  // Setup a camera
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
+  camera.position.set(0, 0, -4);
+  camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('#fff');
+  const directionalLight = new THREE.DirectionalLight('#fff', 1);
+  scene.add(directionalLight);
+
+  // Setup a geometry
+  const geometry = new THREE.SphereGeometry(1, 128, 128);
+
+  // Setup a material
+  const material = new THREE.MeshPhongMaterial({
+    color: '#000',
+    wireframe: true,
+  });
+
+  // Setup a mesh with geometry + material
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  let panningX = false;
+
+  setTimeout(() => {
+    camera.position.z += 2.5;
+    panningX = true;
+  }, 2500);
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      controls.update();
+      geometry.rotateX(0.002);
+      geometry.rotateY(0.002);
+      geometry.rotateZ(0.002);
+      geometry.vertices.forEach(vertex => {
+        vertex.addScalar((Math.random() * 0.01 - 0.005));
+      });
+      geometry.verticesNeedUpdate = true;
+      renderer.render(scene, camera);
+
+      if (panningX) {
+        camera.position.x += 0.005;
+      }
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      controls.dispose();
+      renderer.dispose();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
